ca8a9ce50df708c913e80aa54dae52a9d0648a11
bharathkumar13 <vbk1718@gmail.com>
2023-07-26 14:04:05 +0000

    final commit

--- FinalTestSuite/src/main/java/com/FinalTestSuite/main/repository/OrderRepository.java
+++ FinalTestSuite/src/main/java/com/FinalTestSuite/main/repository/OrderRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.FinalTestSuite.main.entity.OrderEntity;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-    // Custom query methods can be added here if needed
+    
 }
--- FinalTestSuite/src/main/java/com/FinalTestSuite/main/repository/UserRepository.java
+++ FinalTestSuite/src/main/java/com/FinalTestSuite/main/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.FinalTestSuite.main.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Custom query methods can be added here if needed
+   
 }
